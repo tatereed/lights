fc17ea67a39dee1fdb1fc177216e909d4983914f
tatereed <76761406+tatereed@users.noreply.github.com>
2022-09-05 01:56:43 -0400

    small fix

--- lights.py
+++ lights.py
@@ -3,25 +3,25 @@ import time
 from datetime import datetime
 
 def main():
-    lights_on = True
-    while True:
-        daily()
+    test()
 
 def daily():
-    now = datetime.now()
-    if 7 <= int(now.strftime("%H")) <= 21:
-        if lights_on == True:
-            return
+    lights_on = True
+    while True:
+        now = datetime.now()
+        if 7 <= int(now.strftime("%H")) <= 21:
+            if lights_on == True:
+                return
+            else:
+                subprocess.run(["uhubctl", "-l", "1-1", "-a", "on"])
+                lights_on = True
+                return
         else:
-            subprocess.run(["uhubctl", "-l", "1-1", "-a", "on"])
-            lights_on = True
-            return
-    else:
-        if lights_on == False:
-            return
-        else:
-            subprocess.run(["uhubctl", "-l", "1-1", "-a", "off"])
-            lights_on = False
+            if lights_on == False:
+                return
+            else:
+                subprocess.run(["uhubctl", "-l", "1-1", "-a", "off"])
+                lights_on = False
 
 def test():
     lights_on = True
